b430e2e0a3c36658d94834c13fdca75972ce0700
Tom Robinson <tom@tjrobinson.net>
2019-06-14 20:51:58 +0100

    Try to fix routing

--- reading-buses-monitor/src/app/app-routing.module.ts
+++ reading-buses-monitor/src/app/app-routing.module.ts
@@ -1,21 +1,18 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { NextBusComponent } from './next-bus/next-bus.component';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { NextBusComponent } from "./next-bus/next-bus.component";
 
 const routes: Routes = [
   {
-    path: 'next-bus',
+    path: "next-bus",
     component: NextBusComponent,
-    data: { title: 'Next Bus' }
+    data: { title: "Next Bus" }
   },
-  { path: '',
-    redirectTo: '/next-bus',
-    pathMatch: 'full'
-  },
+  { path: "", redirectTo: "/next-bus", pathMatch: "full" }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
