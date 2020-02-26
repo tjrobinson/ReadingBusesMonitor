8d56b8a157b36963b5a22dfc13b964abf993ff52
Tom Robinson <tom@tjrobinson.net>
2020-02-26 20:14:15 +0000

    Tidy-up

--- React/src/App.js
+++ React/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -10,7 +9,6 @@ function App() {
 
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
