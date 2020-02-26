d68bfad1f266679d4e6d22fc4cf4a0164d9bd300
Tom Robinson <tom@tjrobinson.net>
2020-02-26 20:22:16 +0000

    Tidy-up

--- React/src/App.js
+++ React/src/App.js
@@ -5,23 +5,6 @@ function App() {
   return (
     <>
       <a href="https://readingbusesapiproxy.azurewebsites.net/liveJourneys?vehicle=&route=17">Test API Proxy</a>
-   
-
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
     </>
   );
 }
